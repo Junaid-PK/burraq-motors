1737fd583eafaf655b42b72bec614a61664d4daa
Junaid Hussnain <84363665+Junaid-PK@users.noreply.github.com>
2025-09-28 14:32:56 +0500

    ADD: admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Burraq Motors',
+            'email' => 'admin@burraqmotors.co.uk',
+            'password' => Hash::make('password'),           
         ]);
     }
 }
